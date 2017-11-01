45c73bdee1e37788055ed9298a62cbaa552c9ee8
Nikolay Daminov <nd2215@line16.doc.ic.ac.uk>
2017-11-01 17:16:36 +0000

    update

--- ADBCoursework1.cpp
+++ ADBCoursework1.cpp
@@ -20,7 +20,7 @@ std::vector<std::string> findHours(odb::database& db, std::string username) {
 	// Find the hours
 
 	//getting all the users with the mathcing name
-	auto matchingUsers = db.query<user>(odb::query<user>::name == username)
+	auto matchingUsers = db.query<user>(odb::query<user>::name == username);
 
 	//now creating a vector of user IDs
 	std::vector<std::string> userIDs;
