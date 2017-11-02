a2f9f14af033d65c4be96e945af9c7c89440b322
Nikolay Daminov <nd2215@line06.doc.ic.ac.uk>
2017-11-02 19:47:33 +0000

    update

--- ADBCoursework1.cpp
+++ ADBCoursework1.cpp
@@ -87,7 +87,7 @@ void createIndex(odb::database& db){
 	// don't forget to wrap it in a transaction
 	// create a columnstore index to accelerate your query
 	transaction t(db.begin());
-	db.execute("CREATE NONCLUSTERED INDEX business_index ON business(id, latitude, longitude)");
+	db.execute("CREATE NONCLUSTERED INDEX business_index_v2 ON business(id, latitude, longitude)");
 	db.execute("CREATE NONCLUSTERED INDEX review_index ON review(business_id, stars)");
 	t.commit();
 }
@@ -97,7 +97,7 @@ void dropIndex(odb::database& db){
 	// don't forget to wrap it in a transaction
 	// drop the columnstore index you've created
 	transaction t(db.begin());
-	db.execute("DROP INDEX business_id ON business");
+	db.execute("DROP INDEX business_index_v2 ON business");
 	db.execute("DROP INDEX review_index ON review");
 	t.commit();
 }
